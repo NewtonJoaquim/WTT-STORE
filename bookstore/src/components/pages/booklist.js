bce77847c6c62707dce758080ae1241249275497
Newton <newtonjoaquim12@gmail.com>
2019-11-07 00:15:38 -0300

    Captando dados da lista

--- bookstore/src/components/pages/booklist.js
+++ bookstore/src/components/pages/booklist.js
@@ -1,7 +1,18 @@
 import React from "react";
+import data from '../../books.json'
 
 export default class BookList extends React.Component {
+  componentDidMount(){
+    console.log(data.books[0]);
+  }
+  
   render() {
-    return <div>Teste</div>;
+    return (
+    <div>
+      {data.books.map(element => {
+        return element.name
+      })}
+      </div>
+      );
   }
 }
