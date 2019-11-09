1beeb07bef0035c4307bb2f40fdb7c72fe009f38
Newton <newtonjoaquim12@gmail.com>
2019-11-09 11:34:52 -0300

    Feita pagina de detalhes

--- bookstore/src/components/pages/bookDetails.js
+++ bookstore/src/components/pages/bookDetails.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { connect } from 'react-redux';
 import data from '../../books.json'
+import Button from '@material-ui/core/Button';
+
+import { addBook } from "../../actions/shopCart.action";
 
 
 class BookDetails extends React.Component {
@@ -10,19 +13,34 @@ class BookDetails extends React.Component {
         console.log(data.books)
     }
 
+    onAddBookClicked(book){
+        alert("Livro adicionado ao seu carrinho");
+        this.props.addBook(book);
+        this.props.history.push('/booklist');
+      }
+
     render() {
         return (
             <div>
                 {data.books.map(element => {
-                    if(element.code === this.props.match.params.code){
-                        return(
-                            <div>
-                                <li>
-                                    element.name
-                                </li>
+                    if (element.code.toString() === this.props.match.params.code.toString()) {
+
+                        return (
+                            <div key={element.code}>
+                                <h1>
+                                    {element.name}
+                                </h1>
+                                <h2>Preço: {element.price}</h2>
+                                <p>{element.description}</p>
+
+                                <Button variant="outlined" color="primary" onClick={() =>this.onAddBookClicked(element)}>
+                                    Adicionar ao Carrinho
+                                </Button>
                             </div>
                         )
                     }
+                    //return(<div>{element.code}</div>)
+
                 })}
             </div>
         )
@@ -35,4 +53,4 @@ const mapStateToProps = (state) => {
     }
 }
 
-export default connect(mapStateToProps, {})(BookDetails);
+export default connect(mapStateToProps, {addBook})(BookDetails);
--- bookstore/src/components/pages/booklist.js
+++ bookstore/src/components/pages/booklist.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import data from '../../books.json'
 import Button from '@material-ui/core/Button';
 import Fab from '@material-ui/core/Fab';
+import Popover from '@material-ui/core/Popover';
 import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 
 import { addBook } from "../../actions/shopCart.action";
@@ -21,18 +22,17 @@ class BookList extends React.Component {
     <div>
       {data.books.map(element => {
         return (
-          <li key={element.code} style={{padding:10}} onClick={() => this.props.history.push(`/book-details/${element.code}`)}>
-          {element.name}
-          <Button variant="outlined" color="primary">
-            Detalhes
+          <div key={element.code}>
+          {/* <li key={element.code} style={{padding:10}} > */}
+          {/* {element.name} */}
+          <Button variant="outlined" color="primary" onClick={() => this.props.history.push(`/book-details/${element.code}`)}>
+            {element.name}
           </Button>
-          <Button variant="outlined" color="secondary" onClick={() =>this.onAddBookClicked(element)}>
-            Adicionar ao carrinho
-          </Button>
-          </li>
+          {/* </li> */}
+          </div>
           )
       })}
-      <Fab color="primary" aria-label="shop" onClick={() => this.props.history.push('/shopping-cart')}>
+      <Fab color="primary" aria-label="shop" onClick={() => this.props.history.push('/shopping-cart')} onMouseEnter={() => alert('teste')}>
         <ShoppingCartIcon />
       </Fab>
       </div>
